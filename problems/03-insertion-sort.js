d01712954923b5520674da0b6b4f173aa0fb73e1
Laura <lkzailac@gmail.com>
2021-02-23 15:49:34 -0500

    finished insert sort

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -2,7 +2,24 @@
 
 function insertionSort(list) {
   // your code here
+  let holePosition;
+  let valueToInsert;
+
+  for(let i = 1; i < list.length; i++) {
+    valueToInsert = list[i];                                            //V=2
+    holePosition = i;                                                   //H=1
+
+    while(holePosition > 0 && list[holePosition - 1] > valueToInsert) { //1>0true & 3>2true  //0>0false
+      list[holePosition] = list[holePosition - 1];                      //2 => 3
+      holePosition = holePosition - 1;                                  //H=0
+    }
+    list[holePosition] = valueToInsert;                                                      //
+  }
+  return list;
 }
+
+//[3, 2, 1]
+
 
 module.exports = {
   insertionSort
