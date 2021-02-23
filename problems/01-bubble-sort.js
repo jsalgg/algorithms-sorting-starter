00f954dcba88e1b09bb885df7133842e72176a1c
jsalgg <mzootfb@Lukes-MBP.attlocal.net>
2021-02-23 14:09:00 -0500

    selection complete

--- problems/01-bubble-sort.js
+++ problems/01-bubble-sort.js
@@ -19,26 +19,25 @@ function bubbleSort(array) {
   const n = array.length;
   let swapped = true;
 
-  while(swapped) {
+  while (swapped) {
     swapped = false;
     //for(let j = 0; j < n - 1; j++) {
-      for(let i = 0; i < n - 1; i++) {
-        if(array[i] > array[i + 1]) {
-          swap(array, i, i+1);
-          swapped  = true;
-          console.log("inside if", array)
-        }
+    for (let i = 0; i < n - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        swap(array, i, i + 1);
+        swapped = true;
+        //console.log("inside if", array)
       }
+    }
     //}
-
   }
   return array;
 }
 
-let arr= [4, 3, 2, 1];
-console.log(bubbleSort(arr))
+let arr = [4, 3, 2, 1];
+console.log(bubbleSort(arr));
 
 module.exports = {
   bubbleSort,
-  swap
+  swap,
 };
--- problems/02-selection-sort.js
+++ problems/02-selection-sort.js
@@ -3,13 +3,50 @@
 // Implement swap without looking at bubble sort
 function swap(arr, index1, index2) {
   // your code here
+  let holder = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = holder;
 }
 
 function selectionSort(list) {
   // your code here
+  const n = list.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = list[i];
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      if (list[j] < min) {
+        min = list[j];
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      swap(list, i, minIndex);
+      //console.log(list);
+    }
+  }
+
+  return list;
+  /*
+  while (sorted <= list.length) {
+    for (let i = 0; i < list.length - 1; i++) {
+      let num1 = list[i];
+      let num2 = list[i + 1];
+      if (num1 > num2) {
+        swap(list, i, i + 1);
+      }
+    }
+    sorted++;
+  }*/
 }
+
+arr = [4, 3, 2, 1, 5];
+//[4,3,2,1]
+//[1,3,2,4]
+//[1,2,3,4]
+console.log(selectionSort(arr));
 
 module.exports = {
   selectionSort,
-  swap
+  swap,
 };
